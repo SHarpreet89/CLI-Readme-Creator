3370d43fed5b09a170b403a7588f4614723cfb99
SHarpreet89 <S.harpreet@me.com>
2024-07-17 21:05:09 +1000

    Test Inquirer and Generate Markdown.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,32 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Success! Your README.md file has been generated');
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const markdown = generateMarkdown(answers);
+        writeToFile('README.md', markdown);
+    });
+}
 
 // Function call to initialize app
 init();
